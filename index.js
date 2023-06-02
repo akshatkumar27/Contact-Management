caa9561c0f814fd2e23a894aa5c4cd6157c833aa
Akshat kumar <theyashgreat@gmail.com>
2023-06-02 23:53:27 +0530

    bj

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=require('./app');
 const port =process.env.PORT || 3000;
 
 const server = http.createServer(app);
-console.log(process.env.PORT);
+ 
 server.listen(port,() => {
     console.log('Server running on port 3000');
   });
